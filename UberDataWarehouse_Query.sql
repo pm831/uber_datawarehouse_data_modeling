a84e65148076ba9bb7270adb6b67024cb874a6cf
Pujan Malavia (P.J.) <pujanmalavia47@gmail.com>
2024-06-07 18:43:43 -0400

    Add files via upload

---
+++ UberDataWarehouse_Query.sql
@@ -0,0 +1,36 @@
+-- SQLite
+SELECT 
+f.trip_id,
+f.VendorID,
+d.tpep_pickup_datetime,
+d.tpep_dropoff_datetime,
+p.passenger_count,
+t.trip_distance,
+ra.rate_code_name,
+pick.pickup_latitude,
+pick.pickup_longitude,
+dropo.dropoff_latitude,
+dropo.dropoff_longitude,
+pay.payment_type_name,
+f.fare_amount,
+f.extra,
+f.mta_tax,
+f.tip_amount,
+f.tolls_amount,
+f.improvement_surcharge,
+f.total_amount
+FROM fact_table f
+JOIN datetime_dim d  
+  ON f.datetime_id=d.datetime_id
+JOIN passenger_count_dim p  
+  ON p.passenger_count_id=f.passenger_count_id  
+JOIN trip_distance_dim t  
+  ON t.trip_distance_id=f.trip_distance_id  
+JOIN rate_code_dim ra 
+  ON ra.rate_code_id=f.rate_code_id  
+JOIN pickup_location_dim pick 
+  ON pick.pickup_location_id=f.pickup_location_id
+JOIN dropoff_location_dim dropo 
+  ON dropo.dropoff_location_id=f.dropoff_location_id
+JOIN payment_type_dim pay 
+  ON pay.payment_type_id=f.payment_type_id
